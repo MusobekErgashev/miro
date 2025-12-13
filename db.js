937d9fbaf8ffb1f628df81a0797003ba772db72f
MusobekErgashev <musobeccoder007@gmail.com>
2025-12-13 11:15:52 +0500

    db.js is done

---
+++ db.js
@@ -0,0 +1,178 @@
+export const headerLinks = [
+    {
+        linkName: "Product",
+        target: "www.google.com"
+    },
+    {
+        linkName: "Solutions",
+        target: "www.google.com"
+    },
+    {
+        linkName: "Resources",
+        target: "www.google.com"
+    },
+    {
+        linkName: "Enterprise",
+        target: "www.google.com"
+    },
+    {
+        linkName: "Pricing",
+        target: "www.google.com"
+    },
+]
+
+export const trustedBy = [
+    "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/Walmart.png?raw=true",
+    "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/cisco.png?raw=true",
+    "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/Volvo.png?raw=true",
+    "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/deloitte.png?raw=true",
+    "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/okta.png?raw=true"
+]
+
+export const collaborate = [
+    {
+        heading: "Free forever",
+        paragraph: "Our free plan gives you unlimited team members, 3 boards, and 300+ expert-made templates. Signing up with your work email lets you bring in your team faster. See our pricing plans for more features."
+    },
+    {
+        heading: "Easy integrations",
+        paragraph: "Miro has 100+ powerful integrations with tools you already use like G Suite, Slack, and Jira, so your workflow is seamless. View the full list in our Marketplace ."
+    },
+    {
+        heading: "Security first",
+        paragraph: "We treat your data like you would — with the utmost care. We follow industry-leading security standards and give you tools to protect intellectual property. Learn more at our Trust Center ."
+    },
+]
+
+export const buildTheWay = [
+    {
+        linkName: "Brainstorming",
+    },
+    {
+        linkName: "Diagramming"
+    },
+    {
+        linkName: "Meetings & Workshops"
+    },
+    {
+        linkName: "Scrum Events"
+    },
+    {
+        linkName: "Mapping"
+    },
+    {
+        linkName: "Research & Design"
+    },
+]
+
+export const buildAllKinds = [
+    {
+        linkName: "UX & Design",
+    },
+    {
+        linkName: "Marketing"
+    },
+    {
+        linkName: "Product Management"
+    },
+    {
+        linkName: "Engineering"
+    },
+    {
+        linkName: "Consultants"
+    },
+    {
+        linkName: "Agile Coaches"
+    },
+]
+
+export const companiesTrust = [
+    {
+        title: "ISO",
+        subTitle: "ISO-27001 enterprise-grade security compliant"
+    },
+    {
+        title: "#1",
+        subTitle: "Visual Collaboration Platform on G2"
+    },
+    {
+        title: "99%",
+        subTitle: "of the Fortune 100 are customers"
+    },
+    {
+        title: "1,000+",
+        subTitle: "community- and expert-built templates"
+    },
+    {
+        title: "45M+",
+        subTitle: "users around the world"
+    },
+    {
+        title: "100+",
+        subTitle: "integrations with technology partners"
+    },
+]
+
+export const lovedBy = [
+    {
+        title: "vmware",
+        subTitle: "“When the pandemic hit, those of us who thrive on in-person collaboration were worried that our creativity and productivity would suffer. Miro was the perfect tool to help us with collaboration, whiteboarding, and retrospectives while remote.”",
+        image: "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/loved-1.png?raw=true",
+        name: "Roxanne Mustafa",
+        work: "Design Team Lead at VMware"
+    },
+    {
+        title: "DocuSign",
+        subTitle: "“Miro helps solve one of the major gaps in product design: how to manage tasks across product designers whose projects are in different tools.”",
+        image: "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/loved-2.png?raw=true",
+        name: "Jane Ashley",
+        work: "Head of Design at DocuSign"
+    },
+    {
+        title: "frog",
+        subTitle: "“As we used Miro we moved from skepticism to belief to innovation, and now we have a tool that’s at the core of what we do and will continue to extend into the future.”",
+        image: "https://github.com/MusobekErgashev/miro/blob/main/src/assets/icons/loved-3.png?raw=true",
+        name: "Laura Baird",
+        work: "Associate Design Director at frog"
+    },
+]
+
+
+
+export const footerLinks = [
+    {
+        title: "Product",
+        linkName: "Online whiteboard",
+        target: "www.google.com",
+    },
+    {
+        title: "Product",
+        linkName: "Apps & Integrations",
+        target: "www.google.com",
+    },
+    {
+        title: "Product",
+        linkName: "Templates",
+        target: "www.google.com",
+    },
+    {
+        title: "Product",
+        linkName: "Miroverse",
+        target: "www.google.com",
+    },
+    {
+        title: "Product",
+        linkName: "Miro Developer Platform",
+        target: "www.google.com",
+    },
+    {
+        title: "Product",
+        linkName: "Miro for Devices",
+        target: "www.google.com",
+    },
+    {
+        title: "Product",
+        linkName: "Security & Compliance",
+        target: "www.google.com",
+    },
+]
